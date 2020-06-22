29453dac6a15ea074c18f8e352000dfd45a66c6f
Raz <razm47@gmail.com>
2020-06-22 16:02:32 +0300

    change popularity to percents

--- The Movie DB/Views/MovieTableViewCell.swift
+++ The Movie DB/Views/MovieTableViewCell.swift
@@ -13,6 +13,7 @@ class MovieTableViewCell: UITableViewCell {
     //MARK: Properties
     public static let identifier = "movieCell"
     let imageCache = NSCache<NSString, UIImage>()
+    var highestPopularity = 260.0
     
     //MARK: Outlets
     @IBOutlet weak var posterImageView: CustomImageView!
@@ -22,10 +23,21 @@ class MovieTableViewCell: UITableViewCell {
     //MARK: Functions
     public func configureCell(result: Result){
         titleLabel.text = result.title
-        popularityLabel.text = "Popularity: \(result.popularity)"
+//        popularityLabel.text = "Popularity: \(result.popularity)"
+        //popularity in percents:
+        popularityLabel.text = setPopularity(result: result)
         
         posterImageView.loadImageUsingUrlString(urlString: "https://image.tmdb.org/t/p/w440_and_h660_face\(result.posterPath)") { (bool) in
             if !(bool) { print (bool) }
         }
+    }
+    
+    private func setPopularity(result: Result) -> String {
+        if result.popularity > highestPopularity{
+            highestPopularity = result.popularity
+        }
+        
+        let popularity = String(format: "%.2f", result.popularity * 100.0 / highestPopularity)
+        return "Popularity: \(popularity == "100.00" ? "99.99" : popularity)%"
     }
 }
